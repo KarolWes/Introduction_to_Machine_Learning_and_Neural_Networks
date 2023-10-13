b1c210aec5fdbf02d9f46a25788ba4ec658460c0
KarolWes <karol.wesolowski01@gmail.com>
2023-10-13 16:03:59 +0200

    tree

--- main.py
+++ main.py
@@ -56,8 +56,7 @@ def prepare_data():
     return data, outcome
 
 
-if __name__ == '__main__':
-    data, outcome = prepare_data()
+def best_branch(data, outcome):
     results = {}
     for col in data.columns:
         ent, aggr = entropy(data, outcome, col)
@@ -68,11 +67,47 @@ if __name__ == '__main__':
         # print(f"Gain: {col}: {gain}")
         intr, aggr = intrinsic(aggr)
         # print(f"Intrinsic: {col}: {intr}")
-        ratio = gain/intr
+        if intr != 0:
+            ratio = gain / intr
+        else:
+            ratio = 0
         # print(f"Ratio: {ratio}")
         results[col] = ratio
     results = pd.DataFrame.from_dict(results, orient="index")
     results.columns = ["ratio"]
-    results = results.sort_values("ratio")
-    print(results)
-        
+    results = results.sort_values("ratio", ascending=False)
+    return results.head(1).index.values[0]
+
+
+def purity_check(data, outcome, col):
+    used_data = data[col]
+    used_data.name = col
+    used_data = pd.concat([used_data, outcome], axis=1)
+    positives = used_data.groupby(col)['outcome'].sum()
+    counts = used_data.groupby(col)['outcome'].count()
+    used_data = pd.concat([positives, counts], axis=1)
+    used_data.columns = ['positives', 'count']
+    used_data['pure'] = used_data.apply(lambda row: row['positives'] == row['count'] or row['positives'] == 0, axis=1)
+    purity = used_data['pure'].sum() == used_data.shape[0]
+    return purity, used_data
+
+
+def build_tree(data, outcome, depth=0):
+    best = best_branch(data, outcome)
+    purity, res = purity_check(data, outcome, best)
+    for row in res.iterrows():
+        print(depth*'\t' + best + ' - '+str(row[0])+': ')
+        if not row[1]['pure']:
+            new_data = data[data[best] == row[0]]
+            new_data = new_data.drop(best, axis='columns')
+            new_out = outcome[new_data.index]
+            build_tree(new_data, new_out, depth+1)
+        else:
+            print((depth+1)*'\t'+f"decision:{1 if row[1]['positives'] > 0 else 0}")
+    return
+
+
+
+if __name__ == '__main__':
+    data, outcome = prepare_data()
+    build_tree(data, outcome)
